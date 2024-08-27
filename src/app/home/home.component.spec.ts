9ef16f152f0332412d1d59af2d4d93f5d1da4d02
JonaEBM <l20110250@itcj.edu.mx>
2024-08-26 23:15:37 -0700

    Conexión
    Se está trabajando en la conexión con el servidor

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+
+import io from 'socket.io-client';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
