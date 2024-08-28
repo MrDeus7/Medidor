4c30ae347d25d501560ba7e42c7358ee29782236
JonaEBM <l20110250@itcj.edu.mx>
2024-08-27 23:53:36 -0700

    Antes de socket

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,8 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 
-import io from 'socket.io-client';
-
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
